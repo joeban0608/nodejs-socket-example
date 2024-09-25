ae94fe06390c52c60714b3cd8b060d11634d3f57
joeban <joeban@twbeex.com>
2024-09-25 11:05:21 +0800

    feat: frontend add socket to connect server

--- frontend-app/src/app/page.tsx
+++ frontend-app/src/app/page.tsx
@@ -1,5 +1,7 @@
-import Image from "next/image";
+import { io } from "socket.io-client";
 
 export default function Home() {
+  const socket = io("http://localhost:8000");
+
   return <main className="p-4">Home page</main>;
 }
